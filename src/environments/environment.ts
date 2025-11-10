84a40d1003b98f2099ac00d4d56a967efdf6ca7b
anabeatrizarguelho <anabarguelho@gmail.com>
2025-11-10 01:01:36 -0300

    Ocultando credenciais

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,17 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyCEBdZ2l87eVUhpnyshavF8kiFwXOyd1Tk",
-    authDomain: "projeto-contatos-b6561.firebaseapp.com",
-    projectId: "projeto-contatos-b6561",
-    storageBucket: "projeto-contatos-b6561.firebasestorage.app",
-    messagingSenderId: "498672734481",
-    appId: "1:498672734481:web:5e25879a45930a8b953cb0",
-    measurementId: "G-G01JZJVRQ4",
-    databaseURL: "https://projeto-contatos-b6561-default-rtdb.firebaseio.com/"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+    databaseURL: ""
   }
+
+  
 };
 
 /*
